b45a1c5b9f74da4347ebd6394ff90f4ae995113b
elwinspranata <elwinpranata02@gmail.com>
2023-05-25 12:28:04 +0700

    check for the values antered

--- DoubleLingkedlist_022/DoubleLingkedlist_022.cpp
+++ DoubleLingkedlist_022/DoubleLingkedlist_022.cpp
@@ -163,6 +163,52 @@ void DoubleLinkedList::searchData() {
 	}
 }
 
+int main() {
+	DoubleLinkedList obj;
+	while (true) {
+		try {
+			cout << "\nMenu" << endl;
+			cout << "1. Add a record to the list" << endl;
+			cout << "2. Delete a record from the list" << endl;
+			cout << "3. view all records in the asceding order of roll numbers" << endl;
+			cout << "4. view all records in the descending order of roll numbers" << endl;
+			cout << "5. search for a record in the list" << endl;
+			cout << "6. exit" << endl;
+			cout << "\nEnter your choice (1-6): "; 
+				char ch;
+			cin >> ch;
+
+			switch (ch) {
+			case '1':
+				obj.addNode();
+				break;
+			case '2':
+				obj.hapus();
+				break;
+			case '3':
+				obj.ascending();
+				break;
+			case'4':
+				obj.descending();
+				break;
+			case '5':
+				obj.searchData();
+				break;
+			case '6':
+				return 0;
+			default:
+				cout << "\nInvalid option" << endl;
+				break;
+			}
+		}
+		catch (exception& e) {
+			cout << "check for the values antered. " << endl;
+		}
+	}
+}
+
+
+
 
 
 
