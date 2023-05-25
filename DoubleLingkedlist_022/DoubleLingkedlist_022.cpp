5b609172ef01e7a4156e37bf69bdcd4b70363a76
elwinspranata <elwinpranata02@gmail.com>
2023-05-25 10:31:07 +0700

    prevorius data

--- DoubleLingkedlist_022/DoubleLingkedlist_022.cpp
+++ DoubleLingkedlist_022/DoubleLingkedlist_022.cpp
@@ -11,9 +11,18 @@ public:
 };
 
 class DoubleLinkedList {
-private: 
-	Node* START; 
-public: 
+private:
+	Node* START;
+public:
 
-
+	DoubleLinkedList();
+	void addNode();
+	bool search(int rollno, Node** previous, Node** current);
+	bool deleteNode(int rollNo);
+	bool listEmpty();
+	void ascending();
+	void descending();
+	void hapus();
+	void searchData();
 };
+
