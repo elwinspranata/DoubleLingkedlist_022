c5ad17682f188923f8e8d8a5d6be74889021f82d
elwinspranata <elwinpranata02@gmail.com>
2023-05-25 11:34:46 +0700

    Records in

--- DoubleLingkedlist_022/DoubleLingkedlist_022.cpp
+++ DoubleLingkedlist_022/DoubleLingkedlist_022.cpp
@@ -101,6 +101,20 @@ bool DoubleLinkedList::listEmpty() {
 	return (START == NULL);
 }
 
+void DoubleLinkedList::ascending() {
+	if (listEmpty())
+		cout << "\nList is empty" << endl;
+	else {
+		cout << "\nRecords in asceding order of roll number are:" << endl;
+		Node* currentNode = START;
+		while (currentNode != NULL) {
+			cout << currentNode->noMhs << " " << currentNode->name << endl;
+			currentNode = currentNode->next; 
+		}
+	}
+} 
+
+
 
 
 
